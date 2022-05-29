b83cf488124ccfadff8e570f00c841815626d160
Tzvi-H <16guitar@gmail.com>
2022-05-29 11:34:04 -0600

    completed 13.a

--- index.js
+++ index.js
@@ -39,10 +39,22 @@ Note.init(
     modelName: "note",
   }
 );
+Note.sync();
 
 app.get("/api/notes", async (req, res) => {
   const notes = await Note.findAll();
+  console.log(JSON.stringify(notes));
+  console.log(JSON.stringify(notes, null, 2));
   res.json(notes);
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.post("/api/notes", async (req, res) => {
@@ -51,6 +63,17 @@ app.post("/api/notes", async (req, res) => {
     return res.json(note);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
   }
 });
 
